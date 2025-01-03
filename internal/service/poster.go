8601825efd102c210dccd7c6ae99d6b728312dcc
bot <lz18738377974@163.com>
2025-01-03 20:05:31 +0800

    chore: 添加测试代码

--- internal/service/poster.go
+++ internal/service/poster.go
@@ -5,6 +5,7 @@ import (
 	"github.com/go-kratos/kratos/v2/log"
 	"strconv"
 	"sunflower-blog-svc/internal/biz"
+	"time"
 
 	pb "sunflower-blog-svc/api/blog/v1"
 )
@@ -32,8 +33,13 @@ func (s *PosterService) ListPosts(ctx context.Context, req *pb.ListPostsRequest)
 	postBaseInfoList := make([]*pb.PostBaseInfo, 0, len(posts))
 	for _, post := range posts {
 		postBaseInfoList = append(postBaseInfoList, &pb.PostBaseInfo{
-			Title: post.Title,
-			Id:    strconv.Itoa(1),
+			Title:      post.Title,
+			Id:         strconv.Itoa(1),
+			CreatedAt:  int32(time.Now().Unix()),
+			UpdatedAt:  int32(time.Now().Unix()),
+			Tags:       []string{"test", "后端"},
+			Categories: "test",
+			Cover:      "https://hibug.bj.bcebos.com/study-analysis/105217275132383232/105217275132383232_175173842891706368_1_0_1735895898.png?authorization=bce-auth-v1%2FALTAK7asxOyRDVbLWoO1R5U3HM%2F2025-01-03T09%3A18%3A18Z%2F604800%2Fhost%2Fa3965cf84867ad8c3a1c7c483c17ebd97156d54674a5c4b76040ade4a229b539",
 		})
 	}
 
