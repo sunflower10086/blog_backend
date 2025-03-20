c2bb82bd1ab9d9dcd6cc97970cf9c91783519d10
bot <lz18738377974@163.com>
2025-03-20 19:36:06 +0800

    chore: 移动分类分类的方法到新文件

---
+++ app/blog/internal/service/poster/category.go
@@ -0,0 +1,23 @@
+package poster
+
+import (
+	"context"
+	"google.golang.org/protobuf/types/known/emptypb"
+	pb "sunflower-blog-svc/api/blog/v1"
+)
+
+func (s *PosterService) ListCategory(ctx context.Context, req *emptypb.Empty) (*pb.ListCategoryResp, error) {
+	categoryList, err := s.categoryUc.ListCategory(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &pb.ListCategoryResp{}
+	for _, category := range categoryList {
+		resp.Categories = append(resp.Categories, &pb.ListCategoryResp_Category{
+			Id:   int32(category.Id),
+			Name: category.Name,
+		})
+	}
+	return resp, nil
+}
